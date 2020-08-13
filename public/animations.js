fb69f39dc70c71c50c495eb420fb64383b496913
bgunness <brandon.gunness@gmail.com>
2020-08-13 13:20:10 -0400

    Fixed ability to reveal child div when parent quote is clicked

--- public/animations.js
+++ public/animations.js
@@ -1,8 +1,22 @@
 let hidden = document.querySelectorAll('.hidden');
+var quotes = document.querySelectorAll('quote');
 
-$(hidden).hide();
+$(document).ready(() => {
+    $(hidden).hide();
 
-$('.quote').click(() => {
-    console.log($(this));
-    $(hidden).slideDown(1000);
-});
+    $('.yes').click(function() {                                        //Cannot be arrow function for 'this' to work!
+        var hiddenDiv = $(this).children(':first-child');
+        $(hiddenDiv).slideDown(1000);
+    });
+})
+
+/*
+    - click on quote
+        - id of div returned
+    - child of div with class 'hidden' revealed
+
+    or
+
+    - click on quote
+    - first child revealed
+*/
