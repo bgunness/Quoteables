65387e827998e78b6b97928f961be2966ca8697a
bgunness <brandon.gunness@gmail.com>
2020-08-17 13:03:37 -0400

    Fixed input text on modal

--- public/animations.js
+++ public/animations.js
@@ -9,14 +9,14 @@ $(document).ready(() => {
             $('.overlay').hide(200);
         }
     })
-    $('#exampleModalCenter').on('show.bs.modal', function (event) {
-        console.log('hello');
-        var button = $(event.relatedTarget) // Button that triggered the modal
-        var body = button.data('asdf') // Extract info from data-* attributes
-        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-        var modal = $(this)
-        // modal.find('.modal-title').text('New message to ' + recipient)
-        modal.find('.modal-body p').text('running')
-      })
 });
+
+$('#exampleModalCenter').on('show.bs.modal', function (event) {
+    console.log('hello');
+    var button = $(event.relatedTarget)
+    var body = button.data('body')
+    var source = button.data('source')
+    var modal = $(this)
+    modal.find('.input-body').val(body)
+    modal.find('.input-source').val(source)
+    })
