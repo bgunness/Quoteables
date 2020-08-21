9e98ef74f9aff5001b074cb09cbfeff6dc53d0d1
bgunness <brandon.gunness@gmail.com>
2020-08-21 19:07:29 -0400

    Made entire body a flex container on each page

--- public/animations.js
+++ public/animations.js
@@ -27,3 +27,13 @@ $('#modalDeleteQuote').on('show.bs.modal', function (event) {
     var modal = $(this)
     modal.find('#deleteQuoteId').val(id);
 })
+
+// Add slideDown animation to Bootstrap dropdown when expanding.
+$('.dropdown').on('show.bs.dropdown', function() {
+$(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+});
+
+// Add slideUp animation to Bootstrap dropdown when collapsing.
+$('.dropdown').on('hide.bs.dropdown', function() {
+$(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+});
